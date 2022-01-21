db2d6b8471492d777b5ff8bb664248d8c06a4ce9
KZNS <kzns@foxmail.com>
2022-01-22 00:25:45 +0800

    refactor: split handle_paper

--- mdloader.py
+++ mdloader.py
@@ -4,6 +4,8 @@ import logging
 import re
 
 
+# 检查
+
 def assert_warning(e: bool, s: str):
     if not e:
         logging.warning(s)
@@ -21,9 +23,13 @@ def log_error(s: str):
     return assert_error(False, s)
 
 
+# 处理文本
+
 def rbk(s: str):  # remove_blank
     return s
 
+
+# 提取内容
 
 def get_ps(h1):
     ps = []
@@ -44,22 +50,21 @@ def get_content(h1, until_h1):
     return conts
 
 
-def handle_paper(soup: BeautifulSoup):
-    # test
-    with open("out.html", "w") as f:
-        f.write(soup.prettify())
+# 获得每个论文模块
 
-    # metadata
+def get_metadata(soup: BeautifulSoup):
     mete_h1 = soup.find("h1")
     title_cn = rbk(mete_h1.text)  # TODO
     title_en = rbk(mete_h1.find_next_sibling("h2").text)  # TODO
     data_table = mete_h1.find_next_sibling("table").find("tbody")
     data_lines = data_table.find_all("tr")
     data_pairs = [list(map(lambda x: rbk(x.text), i.find_all("td")))
-                  for i in data_lines
-                  ]
+                  for i in data_lines]
     data_dict = dict(data_pairs)  # TODO
+    return "tmp"  # TODO
 
+
+def get_abs(soup: BeautifulSoup):
     # 摘要
     abs_cn_h1 = soup.find("h1", string=re.compile("摘要"))
     ps_key = get_ps(abs_cn_h1)
@@ -76,44 +81,80 @@ def handle_paper(soup: BeautifulSoup):
     ps = ps_key[:-1]  # TODO
     keywords = [rbk(i.text)
                 for i in abs_h1.find_next_sibling("ul").find_all("li")]  # TODO
+    return "tmp"  # TODO
 
-    # 目录
-    pass
 
-    # 引言
+def get_intro(soup: BeautifulSoup):
     intro_h1 = soup.find("h1", string=re.compile("引言"))
     ps = get_ps(intro_h1)  # TODO
+    return "tmp"  # TODO
 
-    # 正文
+
+def get_body(soup: BeautifulSoup):
     body_h1 = soup.find("h1", string=re.compile("正文"))
     content = get_content(body_h1,
                           soup.find("h1", string=re.compile("结论")))
     print(body_h1)  # FIXME
+    return "tmp"  # TODO
 
-    # 结论
+
+def get_conclusion(soup: BeautifulSoup):
     conclusion_h1 = soup.find("h1", string=re.compile("结论"))
     ps = get_ps(conclusion_h1)  # TODO
+    return "tmp"  # TODO
 
-    # 参考文献
+
+def get_reference(soup: BeautifulSoup):
     reference_h1 = soup.find("h1", string=re.compile("参考文献"))
     # 需要一个专门的处理方式
     print(reference_h1)  # FIXME
+    return "tmp"  # TODO
 
-    # 附录
+
+def get_appendix(soup: BeautifulSoup):
     appendix_h1s = soup.find_all("h1", string=re.compile("附录"))
-    content = get_content(appendix_h1s,
+    for i in range(0, len(appendix_h1s)-1):
+        content = get_content(appendix_h1s[i], appendix_h1s[i+1])
+    content = get_content(appendix_h1s[-1],
                           soup.find("h1", string=re.compile("修改记录")))
     print(appendix_h1s)  # FIXME
+    return "tmp"  # TODO
 
-    # 修改记录
+
+def get_record(soup: BeautifulSoup):
     mod_record_h1 = soup.find("h1", string=re.compile("修改记录"))
     content = get_content(mod_record_h1,
                           soup.find("h1", string=re.compile("致谢")))
     print(mod_record_h1)  # FIXME
+    return "tmp"  # TODO
 
-    # 致谢
+
+def get_thanks(soup: BeautifulSoup):
     thanks_h1 = soup.find("h1", string=re.compile("致谢"))
     ps = get_ps(thanks_h1)  # TODO
+    return "tmp"  # TODO
+
+
+# 处理文章
+
+def handle_paper(soup: BeautifulSoup):
+    # test
+    with open("out.html", "w") as f:
+        f.write(soup.prettify())
+
+    data_ls = [
+        get_metadata(soup),    # metadata
+        get_abs(soup),         # 摘要 Abstract
+                               # 目录 pass
+        get_intro(soup),       # 引言
+        get_body(soup),        # 正文
+        get_conclusion(soup),  # 结论
+        get_reference(soup),   # 参考文献
+        get_appendix(soup),    # 附录
+        get_record(soup),      # 修改记录
+        get_thanks(soup)       # 致谢
+    ]
+    return data_ls
 
 
 def handle_translation(soup: BeautifulSoup):
@@ -124,10 +165,7 @@ def load_md(file_name: str, file_type: str):
     with open(file_name, "r") as f:
         md_file = f.read()
     md_html = markdown.markdown(md_file,
-                                extensions=[
-                                    'markdown.extensions.tables'
-                                ]
-                                )
+                                extensions=['markdown.extensions.tables'])
     soup = BeautifulSoup(md_html, 'html.parser')
     for i in soup(text=lambda text: isinstance(text, Comment)):
         i.extract()
