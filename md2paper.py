b0097c72b22949378f15a4d97999b573843cd393
Nathan <tzy15368@outlook.com>
2022-01-21 15:23:02 +0800

    feat: add abstract template render

--- md2paper.py
+++ md2paper.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+from ast import Str
+from curses import raw
 from typing import Union,List
 import docx
-
+from docx.shared import Inches
 class BaseComponent():
     doc_target: docx.Document
 
@@ -11,12 +13,23 @@ class BaseComponent():
     def render_template(self):
         raise NotImplementedError("not implemented")
 
-class Text():
+class BaseContent():
+    def to_paragraph():
+        raise NotImplementedError
+
+class Text(BaseContent):
     # 换行会被该类内部自动处理
     raw_text = ""
 
     def __init__(self, raw_text:str="") -> None:
         self.raw_text = raw_text
+
+    def to_paragraph(self)->str:
+        return self.raw_text
+
+    @classmethod
+    def read(cls, txt:str)->List[Text]:
+        return [Text(i) for i in txt.split('\n')]
 
 class Metadata(BaseComponent):
     school: str = None
@@ -52,6 +65,7 @@ class Metadata(BaseComponent):
         return content
 
     def render_template(self):
+        # 只支持论文，不支持翻译！！
         title_mapping = {
             'zh_CN': 4,
             'en': 5,
@@ -75,20 +89,79 @@ class Metadata(BaseComponent):
             print(len(self.doc_target.paragraphs[line_no].runs[-1].text))
             self.doc_target.paragraphs[line_no].runs[-1].text = self.__fill_blank(BLANK_LENGTH,line_mapping[line_no])
 
-class Abstract():   
-    keyword_zh_CN: Text = None
-    keyword_en: Text = None
-    text_zh_CN: Text = None
-    text_en: Text = None
+class Abstract(BaseComponent):   
+    __keyword_zh_CN: Text = None
+    __keyword_en: Text = None
+    __text_zh_CN: Block = None
+    __text_en: Block = None
 
+    def __init__(self, doc_target: docx.Document) -> None:
+        super().__init__(doc_target)
+        self.__text_en = Block(doc_target)
+        self.__text_zh_CN = Block(doc_target)
+
+    def set_keyword(self, zh_CN:str,en:str):
+        self.__keyword_en = Text(en)
+        self.__keyword_zh_CN = Text(zh_CN)
+
+    def set_text(self, zh_CN:str,en:str):
+        self.__text_en.add_content(content_list=Text.read(en))
+        self.__text_zh_CN.add_content(content_list=Text.read(zh_CN))
+
+    def render_template(self, en_title='this is english title')->None:
+        # 64开始是摘要正文
+        abs_cn_start = 64
+        abs_cn_end = self.__text_zh_CN.render_block(abs_cn_start,title=False)
+        #p = self.doc_target.paragraphs[ABSTRACT_ZH_CN_START].insert_paragraph_before(text=self.text_zh_CN.to_paragraph())
+        
+        # https://stackoverflow.com/questions/30584681/how-to-properly-indent-with-python-docx
+        #p.paragraph_format.first_line_indent = Inches(0.25)
+        
+        while not self.doc_target.paragraphs[abs_cn_end+2].text.startswith("关键词："):
+            p = self.doc_target.paragraphs[abs_cn_end+1]._element
+            p.getparent().remove(p)
+            p._p = p._element = None
+        
+        # cn kw
+        kw_cn_start = abs_cn_end + 2
+        self.doc_target.paragraphs[kw_cn_start].runs[1].text = self.__keyword_zh_CN.to_paragraph()
+        
+        # en start
+
+        en_title_start = kw_cn_start+4
+        self.doc_target.paragraphs[en_title_start].runs[1].text = en_title
+
+        en_abs_start = en_title_start + 3
+        en_abs_end = self.__text_en.render_block(en_abs_start,title=False)-1
+        #self.doc_target.paragraphs[en_abs_start].insert_paragraph_before(text=self.__text_en.to_paragraph())
+
+        # https://stackoverflow.com/questions/61335992/how-can-i-use-python-to-delete-certain-paragraphs-in-docx-document
+        while not self.doc_target.paragraphs[en_abs_end+2].text.startswith("Key Words："):
+            p = self.doc_target.paragraphs[en_abs_end+1]._element
+            p.getparent().remove(p)
+            p._p = p._element = None
+
+        # en kw
+        kw_en_start = en_abs_end +2
+
+        # https://github.com/python-openxml/python-docx/issues/740
+        delete_num = len(self.doc_target.paragraphs[kw_en_start].runs) - 4
+        for run in reversed(list(self.doc_target.paragraphs[kw_en_start].runs)):
+            self.doc_target.paragraphs[kw_en_start]._p.remove(run._r)
+            delete_num -= 1
+            if delete_num < 1:
+                break
+        
+        
+        self.doc_target.paragraphs[kw_en_start].runs[3].text = self.__keyword_en.to_paragraph()
 class Conclusion():
     text_zh_CN: Text = None
 
-class Image():
+class Image(BaseContent):
     img_url = ""
     img_alt = ""
 
-class Formula():
+class Formula(BaseContent):
     pass
 
 class Chapter(): #4
@@ -112,16 +185,52 @@ class ChangeRecord(): #修改记录
 class Acknowledgments(): #致谢
     pass
 
-class Block(): #content
+class Block(BaseComponent): #content
     # 每个block是多个image，formula，text的组合，内部有序
-    title:str = None
-    __content_list__ = List[Union[Text,Image,Formula]]
-    def __init__(self,title:str = "") -> None:
+    
+    def __init__(self, doc_target: docx.Document) -> None:
+        super().__init__(doc_target)
+        self.title:str = None
+        self.__content_list:List[Union[Text,Image,Formula]] = []
+    def set_title(self,title:str) -> None:
         self.title = title
     
-    def add_content(self,content:Union[Text,Image,Formula]) -> Block:
-        self.__content_list__.append(content)
+    def add_content(self,content:Union[Text,Image,Formula]=None,
+            content_list:Union[List[Text],List[Image],List[Formula]]=[]) -> Block:
+        if content:
+            self.__content_list.append(content)
+        for i in content_list:
+            self.__content_list.append(i)
+        #print('added content with len',len(content_list),content_list[0].raw_text,id(self))
         return self
+
+
+    # render_block returns the final paragraph's offset
+    def render_block(self, offset:int, title:bool=True)->int:
+        if not self.__content_list:
+            return
+        if not title:
+            # generate necessary paragraphs
+            internal_content_list = []
+            p = self.doc_target.paragraphs[offset].insert_paragraph_before()
+            internal_content_list.insert(0,p)
+            for i in range(len(self.__content_list)-1):
+                p = internal_content_list[0].insert_paragraph_before()
+                internal_content_list.insert(0,p)
+            
+            #print('got content list',len(internal_content_list),id(self))
+            assert len(self.__content_list)==len(internal_content_list)
+            for i in range(len(self.__content_list)):
+                #p = self.doc_target.paragraphs[offset].insert_paragraph_before()
+                if type(self.__content_list[i]) == Text:
+                    p = internal_content_list[i]
+                    p.text = self.__content_list[i].to_paragraph()
+                    p.paragraph_format.first_line_indent = Inches(0.25)
+                else:
+                    raise NotImplementedError
+            return offset + len(self.__content_list)
+        else:
+            raise NotImplementedError("title unsupported")
 
 class DUTThesisPaper():
     """
@@ -135,6 +244,9 @@ class DUTThesisPaper():
     def setMetadata(self,data:Metadata)->None:
         self.metadata = data
     
+    abstract:Abstract = None
+    def setAbstract(self,abstract:Abstract)->None:
+        self.abstract = abstract
 
     def toDocx():
         pass
@@ -146,15 +258,34 @@ class MD2Paper():
 
 if __name__ == "__main__":
     doc = docx.Document("毕业设计（论文）模板-docx.docx")
-    meta = Metadata(doc_target=doc)
-    meta.school = "电子信息与电气工程"
-    meta.number = "201800000"
-    meta.auditor = "张三"
-    meta.finish_date = "1234年5月6号"
-    meta.teacher = "里斯"
-    meta.major = "计算机科学与技术"
-    meta.title_en = "what is this world"
-    meta.title_zh_CN = "这是个怎样的世界"
-    meta.render_template()
-    doc.save("out.docx")
+    # meta = Metadata(doc_target=doc)
+    # meta.school = "电子信息与电气工程"
+    # meta.number = "201800000"
+    # meta.auditor = "张三"
+    # meta.finish_date = "1234年5月6号"
+    # meta.teacher = "里斯"
+    # meta.major = "计算机科学与技术"
+    # meta.title_en = "what is this world"
+    # meta.title_zh_CN = "这是个怎样的世界"
+    # meta.render_template()
 
+    abs = Abstract(doc)
+    a = """CommonMark中并未定义普通文本高亮。
+CSDN中支持通过==文本高亮==，实现文本高亮
+
+如果你使用的markdown编辑器不支持该便捷设置，可通过HTML标签<mark>实现：
+语法：<mark>文本高亮<mark>
+效果：文本高亮"""
+    b = """代码块中，也支持出现`。与行内代码类似，代码中连续的`，不能超过代码块最外侧的`数量。例如，要打印```（3个），代码块需使用````（4个）声明"""
+    
+    c = """Any subsequent access to the "deleted" paragraph object will raise AttributeError, so you should be careful not to keep the reference hanging around, including as a member of a stored value of Document.paragraphs.
+The reason it's not in the library yet is because the general case is much trickier, in particular needing to detect and handle the variety of linked items that can be present in a paragraph; things like a picture, a hyperlink, or chart etc.
+But if you know for sure none of those are present, these few lines should get the job done."""
+    
+    d = """you should cite the location you copied this from, as acknowledgement of the original author's work."""
+
+    abs.set_text(a,c)
+    abs.set_keyword(b,d)
+    abs.render_template()
+
+    doc.save("out.docx")
