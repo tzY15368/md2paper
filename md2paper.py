86f36e75465c2cd8283b05f0d95d25fdb24ade11
Nathan <tzy15368@outlook.com>
2022-01-22 12:02:07 +0800

    refactor: add doc manager to decouple from renderer

--- md2paper.py
+++ md2paper.py
@@ -1,46 +1,64 @@
 from __future__ import annotations
+from ast import Pass
 from typing import Union,List
 import docx
 from docx.shared import Inches
-        
-class BaseComponent():
-    doc_target: docx.Document
 
-    def __init__(self,doc_target:docx.Document) -> None:
-        self.doc_target = doc_target
+class DocNotSetException(Exception):
+    Pass
+class DocManager():
+    __doc_target = None
 
+    @classmethod
+    def set_doc(cls,doc_target:docx.Document):
+        cls.__doc_target = doc_target
+        cls.__clear_tables()
+
+    @classmethod
+    def get_doc(cls)->docx.Document:
+        if not cls.__doc_target:
+            raise DocNotSetException("doc target is not set, call DM.set_doc")
+        return cls.__doc_target
+
+    @classmethod
+    def __clear_tables(cls):
         # delete all tables on startup as we don't need them
-        for i in range(len(self.doc_target.tables)):
-            t = doc.tables[0]._element
+        for i in range(len(cls.get_doc().tables)):
+            t = cls.get_doc().tables[0]._element
             t.getparent().remove(t)
             t._t = t._element = None
 
-
-    def delete_paragraph_by_index(self, index):
-        p = self.doc_target.paragraphs[index]._element
+    @classmethod
+    def delete_paragraph_by_index(cls, index):
+        p = cls.get_doc().paragraphs[index]._element
         p.getparent().remove(p)
-        p._p = p._element = None
+        p._p = p._element = None    
 
-    def render_template(self):
-        raise NotImplementedError
-
-    def get_anchor_position(self,anchor_text:str)->int:
+    @classmethod
+    def get_anchor_position(cls,anchor_text:str)->int:
         # FIXME: 需要优化
         # 目前被设计成无状态的，只依赖template.docx文件以便测试，增加了性能开销
         # USE-WITH-CARE
         # 只靠标题的anchor-text找paragraph很容易找错，用的时候注意
         i = -1
-        for _i,paragraph in enumerate(self.doc_target.paragraphs):
+        for _i,paragraph in enumerate(cls.get_doc().paragraphs):
             if anchor_text in paragraph.text:
                 i = _i
                 break
         if i==-1: raise ValueError(f"anchor `{anchor_text}` not found") 
         return i + 1
+
+DM = DocManager
+
+class BaseComponent():
+
+    def render_template(self):
+        raise NotImplementedError
+
         
 class Component(BaseComponent):
-    def __init__(self, doc_target: docx.Document) -> None:
-        super().__init__(doc_target)
-        self.__internal_text = Block(doc_target)
+    def __init__(self) -> None:
+        self.__internal_text = Block()
     
     def set_text(self, text:str):
         self.__internal_text.add_content(content_list=Text.read(text))
@@ -50,10 +68,10 @@ class Component(BaseComponent):
     # 直到删除到incr_kw往前incr_next个paragraph
     # incr_kw：见上面incr_next
     def render_template(self, anchor_text:str,  incr_next:int, incr_kw)->int:
-        offset = self.get_anchor_position(anchor_text=anchor_text)
-        while not incr_kw in self.doc_target.paragraphs[offset+incr_next].text\
-                 and (offset+incr_next)!=(len(self.doc_target.paragraphs)-1):
-            self.delete_paragraph_by_index(offset)
+        offset = DM.get_anchor_position(anchor_text=anchor_text)
+        while not incr_kw in DM.get_doc().paragraphs[offset+incr_next].text\
+                 and (offset+incr_next)!=(len(DM.get_doc().paragraphs)-1):
+            DM.delete_paragraph_by_index(offset)
             #print('deleted 1 line for anchor',anchor_text)
         return self.__internal_text.render_block(offset)
 
@@ -74,6 +92,7 @@ class Text(BaseContent):
     @classmethod
     def read(cls, txt:str)->List[Text]:
         return [Text(i) for i in txt.split('\n')]
+
 class Image(BaseContent):
     img_url = ""
     img_alt = ""
@@ -124,8 +143,8 @@ class Metadata(Component):
             'zh_CN': 4,
             'en': 5,
         }
-        self.doc_target.paragraphs[title_mapping['zh_CN']].runs[0].text = self.title_zh_CN
-        self.doc_target.paragraphs[title_mapping['en']].runs[0].text = self.title_en
+        DM.get_doc().paragraphs[title_mapping['zh_CN']].runs[0].text = self.title_zh_CN
+        DM.get_doc().paragraphs[title_mapping['en']].runs[0].text = self.title_en
 
         line_mapping = {
             15: self.school,
@@ -140,8 +159,8 @@ class Metadata(Component):
         for line_no in line_mapping:
             if line_mapping[line_no] == None:
                 continue
-            print(len(self.doc_target.paragraphs[line_no].runs[-1].text))
-            self.doc_target.paragraphs[line_no].runs[-1].text = self.__fill_blank(BLANK_LENGTH,line_mapping[line_no])
+            print(len(DM.get_doc().paragraphs[line_no].runs[-1].text))
+            DM.get_doc().paragraphs[line_no].runs[-1].text = self.__fill_blank(BLANK_LENGTH,line_mapping[line_no])
 
 class Abstract(Component):   
     __keyword_zh_CN: Text = None
@@ -149,10 +168,9 @@ class Abstract(Component):
     __text_zh_CN: Block = None
     __text_en: Block = None
 
-    def __init__(self, doc_target: docx.Document) -> None:
-        super().__init__(doc_target)
-        self.__text_en = Block(doc_target)
-        self.__text_zh_CN = Block(doc_target)
+    def __init__(self) -> None:
+        self.__text_en = Block()
+        self.__text_zh_CN = Block()
 
     def set_keyword(self, zh_CN:List[str],en:List[str]):
         SEPARATOR = "；"
@@ -172,39 +190,39 @@ class Abstract(Component):
         # https://stackoverflow.com/questions/30584681/how-to-properly-indent-with-python-docx
         #p.paragraph_format.first_line_indent = Inches(0.25)
         
-        while not self.doc_target.paragraphs[abs_cn_end+2].text.startswith("关键词："):
-            self.delete_paragraph_by_index(abs_cn_end+1)
+        while not DM.get_doc().paragraphs[abs_cn_end+2].text.startswith("关键词："):
+            DM.delete_paragraph_by_index(abs_cn_end+1)
         
         # cn kw
         kw_cn_start = abs_cn_end + 2
-        self.doc_target.paragraphs[kw_cn_start].runs[1].text = self.__keyword_zh_CN.to_paragraph()
+        DM.get_doc().paragraphs[kw_cn_start].runs[1].text = self.__keyword_zh_CN.to_paragraph()
         
         # en start
 
         en_title_start = kw_cn_start+4
-        self.doc_target.paragraphs[en_title_start].runs[1].text = en_title
+        DM.get_doc().paragraphs[en_title_start].runs[1].text = en_title
 
         en_abs_start = en_title_start + 3
         en_abs_end = self.__text_en.render_block(en_abs_start)-1
         #self.doc_target.paragraphs[en_abs_start].insert_paragraph_before(text=self.__text_en.to_paragraph())
 
         # https://stackoverflow.com/questions/61335992/how-can-i-use-python-to-delete-certain-paragraphs-in-docx-document
-        while not self.doc_target.paragraphs[en_abs_end+2].text.startswith("Key Words："):
-            self.delete_paragraph_by_index(en_abs_end+1)
+        while not DM.get_doc().paragraphs[en_abs_end+2].text.startswith("Key Words："):
+            DM.delete_paragraph_by_index(en_abs_end+1)
 
         # en kw
         kw_en_start = en_abs_end +2
 
         # https://github.com/python-openxml/python-docx/issues/740
-        delete_num = len(self.doc_target.paragraphs[kw_en_start].runs) - 4
-        for run in reversed(list(self.doc_target.paragraphs[kw_en_start].runs)):
-            self.doc_target.paragraphs[kw_en_start]._p.remove(run._r)
+        delete_num = len(DM.get_doc().paragraphs[kw_en_start].runs) - 4
+        for run in reversed(list(DM.get_doc().paragraphs[kw_en_start].runs)):
+            DM.get_doc().paragraphs[kw_en_start]._p.remove(run._r)
             delete_num -= 1
             if delete_num < 1:
                 break
         
         
-        self.doc_target.paragraphs[kw_en_start].runs[3].text = self.__keyword_en.to_paragraph()
+        DM.get_doc().paragraphs[kw_en_start].runs[3].text = self.__keyword_en.to_paragraph()
         return kw_en_start+1
 class Conclusion(Component):
     def render_template(self) -> int:
@@ -221,11 +239,11 @@ class References(Component): #参考文献
         ANCHOR = "参 考 文 献"
         incr_next = 1
         incr_kw = "附录A"
-        offset_start = self.get_anchor_position(ANCHOR)
+        offset_start = DM.get_anchor_position(ANCHOR)
         offset_end = super().render_template(ANCHOR, incr_next, incr_kw) -incr_next+1
-        _style = self.doc_target.styles['参考文献正文']
+        _style = DM.get_doc().styles['参考文献正文']
         for i in range(offset_start,offset_end):
-            self.doc_target.paragraphs[i].style = _style
+            DM.get_doc().paragraphs[i].style = _style
 
         
 
@@ -254,8 +272,7 @@ class Acknowledgments(Component): #致谢
 class Block(BaseComponent): #content
     # 每个block是多个image，formula，text的组合，内部有序
     
-    def __init__(self, doc_target: docx.Document) -> None:
-        super().__init__(doc_target)
+    def __init__(self) -> None:
         self.__title:str = None
         self.__content_list:List[Union[Text,Image,Formula]] = []
         self.__sub_blocks:List[Block] = []
@@ -286,7 +303,7 @@ class Block(BaseComponent): #content
     def render_template(self, offset:int)->int:
         new_offset = offset
         if self.__title:
-            p_title = self.doc_target.paragraphs[offset].insert_paragraph_before()
+            p_title = DM.get_doc().paragraphs[offset].insert_paragraph_before()
             p_title.style = doc.styles['Heading 1']
             p_title.add_run()
             p_title.runs[0].text= str(self.__id) if self.__id else "" +"  "+self.__title
@@ -309,7 +326,7 @@ class Block(BaseComponent): #content
             return offset
         # generate necessary paragraphs
         internal_content_list = []
-        p = self.doc_target.paragraphs[offset].insert_paragraph_before()
+        p = DM.get_doc().paragraphs[offset].insert_paragraph_before()
         internal_content_list.insert(0,p)
         for i in range(len(self.__content_list)-1):
             p = internal_content_list[0].insert_paragraph_before()
@@ -318,10 +335,9 @@ class Block(BaseComponent): #content
         #print('got content list',len(internal_content_list),id(self))
         assert len(self.__content_list)==len(internal_content_list)
         for i in range(len(self.__content_list)):
-            #p = self.doc_target.paragraphs[offset].insert_paragraph_before()
             if type(self.__content_list[i]) == Text:
                 p = internal_content_list[i]
-                p.style = self.doc_target.styles['Normal']
+                p.style = DM.get_doc().styles['Normal']
                 p.text = self.__content_list[i].to_paragraph()
                 p.paragraph_format.first_line_indent = Inches(0.25)
             else:
@@ -353,7 +369,9 @@ class MD2Paper():
         pass
 
 if __name__ == "__main__":
+    
     doc = docx.Document("毕业设计（论文）模板-docx.docx")
+    DM.set_doc(doc)
     # meta = Metadata(doc_target=doc)
     # meta.school = "电子信息与电气工程"
     # meta.number = "201800000"
@@ -365,7 +383,7 @@ if __name__ == "__main__":
     # meta.title_zh_CN = "这是个怎样的世界"
     # meta.render_template()
 
-    abs = Abstract(doc)
+    abs = Abstract()
     a = """CommonMark中并未定义普通文本高亮。
 CSDN中支持通过==文本高亮==，实现文本高亮
 
@@ -384,7 +402,7 @@ But if you know for sure none of those are present, these few lines should get t
     abs.set_keyword(b,d)
     abs.render_template()
 
-    conc = Conclusion(doc)
+    conc = Conclusion()
     e = """如果代码中出现太多的条件判断语句的话，代码就会变得难以维护和阅读。 这里的解决方案是将每个状态抽取出来定义成一个类。
 这里看上去有点奇怪，每个状态对象都只有静态方法，并没有存储任何的实例属性数据。 实际上，所有状态信息都只存储在 Connection 实例中。 
 在基类中定义的 NotImplementedError 是为了确保子类实现了相应的方法。 这里你或许还想使用8.12小节讲解的抽象基类方式。
@@ -393,19 +411,19 @@ But if you know for sure none of those are present, these few lines should get t
     conc.set_text(e)
     conc.render_template()
 
-    ref = References(doc)
+    ref = References()
     h = """[1] 国家标准局信息分类编码研究所.GB/T 2659-1986 世界各国和地区名称代码[S]//全国文献工作标准化技术委员会.文献工作国家标准汇编:3.北京:中国标准出版社,1988:59-92. 
 [2] 韩吉人.论职工教育的特点[G]//中国职工教育研究会.职工教育研究论文集.北京:人民教育出版社,1985:90-99. """
     ref.set_text(h)
     ref.render_template()
 
-    ack = Acknowledgments(doc)
+    ack = Acknowledgments()
     f = """肾衰竭（Kidney failure）是一种终末期的肾脏疾病，此时肾脏的功能会低于其正常水平的15%。由于透析会严重影响患者的生活质量，肾移植一直是治疗肾衰竭的理想方式。但肾脏供体一直处于短缺状态，移植需等待时间约为5-10年。近日，据一篇发表于《美国移植杂志》的文章，阿拉巴马大学伯明翰分校的科学家首次成功将基因编辑猪的肾脏成功移植给一名脑死亡的人类接受者。
 研究使用的供体猪的10个关键基因经过了基因编辑，其肾脏的功能更适合人体，且植入人体后引发的免疫排斥反应更轻微。研究人员首先对异种供体和接受者进行交叉配型测试。经配对后，研究人员将肾脏移植入脑死亡接受者的肾脏对应的解剖位置，与肾动脉、肾静脉和输尿管相连接。移植后，他们为患者进行了常规的免疫抑制治疗。目前，肾脏已在患者体内正常工作77小时。该研究按照1期临床试验标准进行，完全按人类供体器官的移植标准实施。研究显示，异种移植的发展在未来或可缓解世界范围器官供应压力。"""
     ack.set_text(f)
     ack.render_template()
 
-    cha = ChangeRecord(doc)
+    cha = ChangeRecord()
     g = """在无线传能技术中，非辐射无线传能（即电磁感应充电）可以高效传输能量，但有效传输距离被限制在收发器尺寸的几倍之内；而辐射无线传能（如无线电、激光）虽然可以远距离传输能量，但需要复杂的控制机制来跟踪移动的能量接收器。
 近日，同济大学电子与信息工程学院的研究团队通过理论和实验证明，"""
     cha.set_text(g)
@@ -413,3 +431,5 @@ But if you know for sure none of those are present, these few lines should get t
     #cha.render_template()
 
     doc.save("out.docx")
+
+    
