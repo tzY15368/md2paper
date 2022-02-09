b5683d515b6d45fffe156f0d206e5dce772a0313
Nathan <tzy15368@outlook.com>
2022-02-09 12:00:51 +0800

    chore: add version

--- setup_wasm.py
+++ setup_wasm.py
@@ -1,18 +1,16 @@
 from setuptools import setup
+
 from md2paper.version import __version__
+
 # repeated requirements is error-prone
 REQUIREMENTS = [
-    'python-docx==0.8.11',
+    #'python-docx==0.8.11',
     'latex2mathml==3.63.3',
     'Markdown==3.3.6',
     'beautifulsoup4>=4.9.3',
-    'bibtexparser==1.2.0',
+    #'bibtexparser==1.2.0',
     'Pillow==9.0.0'
 ]
-
-# python-docx和bibtexparser在wasm环境下无法直接安装(缺少直接的wheel)，
-# 在安装md2paper前应该手动安装以上依赖项
-# 选项之一是使用我们手动打包的.whl
 
 setup(
     name = 'md2paper',
