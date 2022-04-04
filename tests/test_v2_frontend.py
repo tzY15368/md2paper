d7b90f7bc2b469a93be756ca07b3fc6c4f1ec9ec
KZNS <kzns@foxmail.com>
2022-04-04 21:48:21 +0800

    feat: delete table of contents block

--- tests/test_v2_frontend.py
+++ tests/test_v2_frontend.py
@@ -3,7 +3,7 @@ sys.path.append('..')
 from md2paper.v2.frontend import Paper, DUTPaperPreprocessor
 import logging
 
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.WARNING)
 
 if __name__ == '__main__':
     p = Paper("../example/论文.md", DUTPaperPreprocessor)
